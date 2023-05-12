3ee1b5a6b5dc82205e8d58044a49fead89ef9088
Hebert <hebertLandim007@outlook.com>
2023-05-12 10:21:15 -0300

    Move games on list

--- dslist/src/main/java/com/devsuperior/dslist/services/GameService.java
+++ dslist/src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -32,8 +32,8 @@ public class GameService {
 
 	@Transactional(readOnly = true)
 	public List<GameMinDTO> findByGameList(Long listId) {
-		List<GameMinProjection> games = gameRepository.searchByList(listId);
-		return games.stream().map(x -> new GameMinDTO(x)).toList();
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
 	}
 
 }
